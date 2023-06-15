db7a88ffd12a6d7aaf9c06eb1b1107ebd27f321e
Leonardo Alves <leonardogomesalves1@gmail.com>
2023-06-15 17:55:30 +0100

    Adicionei os primeiros exercícios que fiz sobre listas e coisas básicas

---
+++ listas.py
@@ -0,0 +1,37 @@
+# a.append -> adiciona no final -> aloca espaço automaticamente
+# a.count(x) -> conta o numero de vezes que x aparece na lista
+# a.insert(i, x) -> insere x na posição i da lista
+# a.remove(x) -> remove a primeira aparição de x
+# a.clear -> remove tudo da lista
+# a.index (x, [start, end]) -> dá o index de x dado um intervalo
+# a.sort()
+
+def swap(a, i, j):
+    save = a[i]
+    a[i] = a[j]
+    a[j] = save
+
+def soma_lista(a, N):
+    soma = 0
+    i = 0
+    while i < N:
+        soma += a[i]
+        i+=1
+    return soma
+
+def quadrados(a, N):
+    i = 0
+    valor = 0
+    while i < N:
+        lista.append(valor ** 2)
+        valor+=1
+        i+=1
+    return lista
+
+
+lista = []
+#swap(lista, 0, 3)
+#lista.reverse()
+quadrados(lista, 4)
+
+print(lista)
---
+++ primeiros_exercicios.py
@@ -0,0 +1,64 @@
+def quadrado(n):
+    i = 0
+    while i < n:
+        j = 0
+        while j < n:
+            if j+1 < n:
+                print("#", end= "")
+            else:
+                print("#")
+            j += 1
+
+        i+=1
+
+def xadrez(n):
+    linhas = 0
+    while linhas < n:
+        linha = 0
+        if (linhas % 2) == 0:
+            linha = 0
+            while linha < n:
+                if linha % 2 == 0:
+                    print("#", end= "")
+                else:
+                    print ("_", end="")
+                linha+=1
+            print("")
+            linhas+=1
+
+        else:
+            linha = 0
+            while linha < n:
+                if linha % 2 == 0:
+                    print("_", end="")
+                else:
+                    print("#", end="")
+                linha+=1
+            print("")
+            linhas+=1
+
+def triangulo_horizontal(n):
+    linhas = 0
+    while linhas < n+1:
+        i = 0
+        while i < linhas:
+            print("#", end="")
+            i+=1
+        print("")
+        linhas+=1
+    baixo = n-1
+    while baixo > 0:
+        i = 0
+        while i < baixo:
+            print("#", end="")
+            i+=1
+        print("")
+        baixo-=1
+
+def triangulo_vertical (n):
+
+triangulo_horizontal(2)
+print("")
+quadrado(5)
+print("")
+xadrez(5)
